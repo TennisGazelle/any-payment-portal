ce702d63198dbe4c502951642ded3985a73179cf
Daniel Lopez <daniel.lopez@nocapshows.com>
2023-02-10 13:42:39 -0800

    added datepicker with 16th restriction

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react';
+
+import dayjs, { Dayjs } from 'dayjs';
+import TextField from '@mui/material/TextField';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { StaticDatePicker } from '@mui/x-date-pickers/StaticDatePicker';
+import { Tooltip } from '@mui/material';
+
 import './App.css';
 
+
 function App() {
+  const [value, setValue] = useState<Dayjs | null>(dayjs());
+
+  const isWeekend = (date: Dayjs) => {
+    const day = date.day();
+    return day === 0 || day === 6;
+  }
+
+  const isAfter16th = (date: Dayjs) => {
+    return date.date() >= 17;
+  }
+
+  const renderToolTipOnDay = (day: any, selectedDates: any, dayInCurrentMonth: any) => {
+    if (day.getDate() >= 17) {
+      return (
+        <Tooltip title="Cannot Pick Date, Late Fees Incur on the 16th">
+          <div>{day.getDate()}</div>
+        </Tooltip>
+      );
+    }
+    return(
+      <div>{dayInCurrentMonth}</div>
+    )
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <StaticDatePicker
+          orientation="landscape"
+          openTo="day"
+          value={value}
+          shouldDisableDate={isAfter16th}
+          // renderDay={renderToolTipOnDay}
+          onChange={(newValue) => {
+            setValue(newValue);
+          }}
+          renderInput={(params) => <TextField {...params} />}
+        />
+      </LocalizationProvider>
       </header>
     </div>
   );
